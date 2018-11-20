43bcd413ab19313f497f5a2d14266c7d5e124cb0
Thomas Burr <tb4cmcle@gmail.com>
2018-11-20 16:23:33 -0700

    starting

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+
+const app = express();
+const port = process.env.PORT || 5000;
+const dbConnectionString = process.env.DATABASE_URL || "something";
+
+app.get("/person", getPerson);
+
+app.listen(port, function() {
+   console.log("server is listening on port" + porsublt);
+});
+
+function getPerson(req,res) {
+   console.log("getting person...");
+
+   // get id from req
+   console.log("Trying to connect to a db at: " + dbConnectionString);
+
+   res.json({name: "john"});
+}
