0bcf7855688d0a7b8262236d930a4b57728becea
Thomas Burr <tb4cmcle@gmail.com>
2018-11-20 16:32:03 -0700

    stuff

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const dbConnectionString = process.env.DATABASE_URL || "something";
 app.get("/person", getPerson);
 
 app.listen(port, function() {
-   console.log("server is listening on port" + porsublt);
+   console.log("server is listening on port" + port);
 });
 
 function getPerson(req,res) {
