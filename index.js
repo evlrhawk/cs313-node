6bcc1284dfa69b3e3a6268487877320d73e3e820
Thomas Burr <tb4cmcle@gmail.com>
2018-11-24 14:00:59 -0700

    attempting stuff

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
 const dbConnectionString = process.env.DATABASE_URL || "something";
-const { Pool} = require('pg')
-const pool = new Pool();
+const { Pool} = require('pg');
+const pool = new Pool({dbConnectionString: dbConnectionString});
 
 app.get("/person", getPerson);
 
@@ -18,12 +18,28 @@ function getPerson(req,res) {
    // get id from req
    console.log("Trying to connect to a db at: " + dbConnectionString);
 
-   pool.query('SELECT * FROM person', (err, res) => {
-      if (err) {
-       throw err
-      }
+   getPersonFromDB(id, function(err, res){
+      console.log("");
+   });
+   
 
-   res.json(res.rows[0]);
-   console.log('user:', res.rows[0])
-   })
+   res.json(res);
+   
+}
+
+function getPersonFromDB(id, callback){
+     var sql = "SELECT * FROM person WHERE id = $1::int";
+     var params = [id];
+
+     pool.query(sql, params, function(err, res){
+      if (err){
+         console.log("log.wtf" + err);
+         callback(err, null);
+      }
+     
+      callback(null, result.rows);
+     
+     });
+
+
 }
