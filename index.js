64699a68bc270eb72646d85571ff428d6a5dc786
Thomas Burr <tb4cmcle@gmail.com>
2018-12-03 16:49:57 -0700

    adding html

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(express.static(__dirname + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({extended : true}));
 
-app.get("/user", controller.getUser);
+app.get("/public/user", controller.getUser);
 app.post("/user", controller.createUser);
 
 // app.get("/category", controller.getCategory);
