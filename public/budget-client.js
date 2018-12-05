57139b0d47ace9841e08a8e6edaf0c88e4e6e9f6
Thomas Burr <tb4cmcle@gmail.com>
2018-12-05 12:39:59 -0700

    fixing get

--- public/budget-client.js
+++ public/budget-client.js
@@ -7,7 +7,7 @@ function getUser() {
    var user = document.getElementById("main");
    console.log("Made it");
 // Get query string for request
-   var string = "/user?" + username + "&" + pwd;
+   var string = "../user?" + username + "&" + pwd;
 
    console.log(string);
    var xhttp = new XMLHttpRequest();
