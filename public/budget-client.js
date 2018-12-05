54535ecbcbf79df377bc918bf9db93148bb505e9
Thomas Burr <tb4cmcle@gmail.com>
2018-12-04 21:58:29 -0700

    adding client side js page

--- public/budget-client.js
+++ public/budget-client.js
@@ -20,6 +20,7 @@ function getUser() {
          // }
          console.log(string1);
          document.getElementById("main").innerHTML = obj;
+         console.log(obj);
     };
     xhttp.open("GET", string, true);
     xhttp.send();
